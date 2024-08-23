e576955890c50e984217cc713be67aea5ed4d001
jjub0217 <noonssub1004@gmail.com>
2024-08-24 03:59:34 +0900

    메인페이지, live 페이지, 이용권 쿠폰 페이지, live 컨텐츠 상세 페이지 분리 및 스타일 변경

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,25 +1,32 @@
-const mainVisual = new Swiper(".section_schedule .swiper", {
-  a11y: {
-    enabled: true,
-    containerMessage: "프로모션 슬라이드 영역입니다.",
-    slideLabelMessage:
-      "총 {{slidesLength}}장의 슬라이드 중 {{index}}번 슬라이드 입니다.",
-    firstSlideMessage: "첫번째 슬라이드입니다.",
-    lastSlideMessage: "마지막 슬라이드입니다.",
-    paginationBulletMessage: "{{index}}번째 슬라이드로 이동합니다.",
-    containerRoleDescriptionMessage: "Carousel",
-    itemRoleDescriptionMessage: "Slide",
-    slideRole: "listitem",
-  },
-  slidesPerView: 4,
-  pagination: {
-    el: ".pagination",
-  },
-    navigation : {
-      nextEl: ".section_schedule .btn_next",
-      prevEl: ".section_schedule .btn_prev"
-    },
-});
+const createSwiper = (selector, options) => new Swiper(selector, options);
+
+  createSwiper(".section_visual .swiper", {
+    pagination: { el: ".pagination", type: "fraction" },
+    slidesPerView: 3,
+    spaceBetween: 12,
+  });
+
+// const mainVisual = new Swiper(".section_visual .swiper", {
+//   // a11y: {
+//   //   // enabled: true,
+//   //   //   containerMessage: "프로모션 슬라이드 영역입니다.",
+//   //   //   slideLabelMessage:
+//   //   //     "총 {{slidesLength}}장의 슬라이드 중 {{index}}번 슬라이드 입니다.",
+//   //   //   firstSlideMessage: "첫번째 슬라이드입니다.",
+//   //   //   lastSlideMessage: "마지막 슬라이드입니다.",
+//   //   //   paginationBulletMessage: "{{index}}번째 슬라이드로 이동합니다.",
+//   //   //   containerRoleDescriptionMessage: "Carousel",
+//   //   //   itemRoleDescriptionMessage: "Slide",
+//   //   //   slideRole: "listitem",
+//   //   // },
+//   //   // navigation: {
+//   //   //   nextEl: ".section_event .btn_next",
+//   //   //   prevEl: ".section_event .btn_prev",
+//   // },
+//   pagination: { el: ".pagination", type: "fraction" },
+//   slidesPerView: 4,
+//   spaceBetween: 12,
+// });
 
 const eventVisual = new Swiper(".section_event .swiper", {
   a11y: {
@@ -38,11 +45,11 @@ const eventVisual = new Swiper(".section_event .swiper", {
   pagination: {
     el: ".pagination",
   },
-  navigation : {
+  navigation: {
     nextEl: ".section_event .btn_next",
-    prevEl: ".section_event .btn_prev"
+    prevEl: ".section_event .btn_prev",
   },
-  spaceBetween: 12
+  spaceBetween: 12,
 });
 
 const bestArtistVisual = new Swiper(".section_best_artist .swiper", {
@@ -62,11 +69,11 @@ const bestArtistVisual = new Swiper(".section_best_artist .swiper", {
   pagination: {
     el: ".pagination",
   },
-  navigation : {
+  navigation: {
     nextEl: ".section_best_artist .btn_next",
-    prevEl: ".section_best_artist .btn_prev"
+    prevEl: ".section_best_artist .btn_prev",
   },
-  spaceBetween: 12
+  spaceBetween: 12,
 });
 
 const artistVisual = new Swiper(".section_artist .artist_area .swiper", {
@@ -86,48 +93,44 @@ const artistVisual = new Swiper(".section_artist .artist_area .swiper", {
   pagination: {
     el: ".pagination",
   },
-  navigation : {
+  navigation: {
     nextEl: ".section_artist .btn_next",
-    prevEl: ".section_artist .btn_prev"
+    prevEl: ".section_artist .btn_prev",
   },
-  spaceBetween: 12
+  spaceBetween: 12,
 });
-
-
 
 const pushButtons = document.querySelectorAll(".push_notification_btn");
 const selectBoxes = document.querySelectorAll(".select_box");
 const uploadMoreBtn = document.querySelectorAll(".more_btn");
 
-
 pushButtons.forEach((pushButton) => {
   pushButton.onclick = (e) => {
     const isPressed = pushButton.getAttribute("aria-pressed") === "true";
     pushButton.setAttribute("aria-pressed", !isPressed);
-  }
+  };
 });
 
-
-uploadMoreBtn.forEach(btn => {
+uploadMoreBtn.forEach((btn) => {
   btn.onclick = (e) => {
     const hiddenItems = [...e.target.previousElementSibling.children];
-    hiddenItems.forEach(item => {
+    hiddenItems.forEach((item) => {
       if (item.classList.contains("is_hidden")) {
-        item.classList.remove("is_hidden")
+        item.classList.remove("is_hidden");
       }
-    })
+    });
     e.target.previousElementSibling.ariaExpanded = "true";
     e.target.classList.add("is_hidden");
-  }
-})
-
+  };
+});
 
 selectBoxes.forEach((selectBox) => {
   const selectBtn = selectBox.querySelector(".option_title");
   const optionButtons = selectBox.querySelectorAll('button[role="option"]');
 
   selectBtn.onclick = (e) => {
-    e.target.ariaExpanded = e.target.ariaExpanded === "false" ? "true" : "false";
+    e.target.ariaExpanded =
+      e.target.ariaExpanded === "false" ? "true" : "false";
   };
 
   optionButtons.forEach((button) => {
---
+++ assets/js/live.js
@@ -0,0 +1,36 @@
+const scheduleVisual = new Swiper(".section_schedule .swiper", {
+  a11y: {
+    enabled: true,
+    containerMessage: "프로모션 슬라이드 영역입니다.",
+    slideLabelMessage:
+      "총 {{slidesLength}}장의 슬라이드 중 {{index}}번 슬라이드 입니다.",
+    firstSlideMessage: "첫번째 슬라이드입니다.",
+    lastSlideMessage: "마지막 슬라이드입니다.",
+    paginationBulletMessage: "{{index}}번째 슬라이드로 이동합니다.",
+    containerRoleDescriptionMessage: "Carousel",
+    itemRoleDescriptionMessage: "Slide",
+    slideRole: "listitem",
+  },
+  slidesPerView: 4,
+  navigation: {
+    nextEl: ".section_schedule .btn_next",
+    prevEl: ".section_schedule .btn_prev",
+  },
+});
+
+const tabItems = document.querySelectorAll(".tab_item");
+
+tabItems.forEach((item) => {
+  item.onclick = (e) => {
+    e.preventDefault();
+    [...tabItems].filter((item) => {
+      if (item === e.target.parentNode) {
+        e.target.parentNode.classList.add("is_active");
+        e.target.parentNode.ariaSelected = "true";
+      } else {
+        item.classList.remove("is_active");
+        item.ariaSelected = "false";
+      }
+    });
+  };
+});
