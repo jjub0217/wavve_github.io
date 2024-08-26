df70d1bb1db56cb0e11401604a395528aaae56bf
jjub0217 <noonssub1004@gmail.com>
2024-08-26 14:37:27 +0900

    메인페이지의 메인비쥬얼 스와이퍼 코드 개선, 메인페이지의 메인비쥬얼 스타일 변경, fraction 수정

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,50 +1,39 @@
+
+const commonA11ySettings = {
+  enabled: true,
+  containerMessage: "프로모션 슬라이드 영역입니다.",
+  slideLabelMessage:
+    "총 {{slidesLength}}장의 슬라이드 중 {{index}}번 슬라이드 입니다.",
+  firstSlideMessage: "첫번째 슬라이드입니다.",
+  lastSlideMessage: "마지막 슬라이드입니다.",
+  paginationBulletMessage: "{{index}}번째 슬라이드로 이동합니다.",
+  containerRoleDescriptionMessage: "Carousel",
+  itemRoleDescriptionMessage: "Slide",
+  slideRole: "listitem",
+};
+
 const createSwiper = (selector, options) => new Swiper(selector, options);
 
-  createSwiper(".section_visual .swiper", {
-    pagination: { el: ".pagination", type: "fraction" },
-    slidesPerView: 3,
-    spaceBetween: 12,
-  });
-
-// const mainVisual = new Swiper(".section_visual .swiper", {
-//   // a11y: {
-//   //   // enabled: true,
-//   //   //   containerMessage: "프로모션 슬라이드 영역입니다.",
-//   //   //   slideLabelMessage:
-//   //   //     "총 {{slidesLength}}장의 슬라이드 중 {{index}}번 슬라이드 입니다.",
-//   //   //   firstSlideMessage: "첫번째 슬라이드입니다.",
-//   //   //   lastSlideMessage: "마지막 슬라이드입니다.",
-//   //   //   paginationBulletMessage: "{{index}}번째 슬라이드로 이동합니다.",
-//   //   //   containerRoleDescriptionMessage: "Carousel",
-//   //   //   itemRoleDescriptionMessage: "Slide",
-//   //   //   slideRole: "listitem",
-//   //   // },
-//   //   // navigation: {
-//   //   //   nextEl: ".section_event .btn_next",
-//   //   //   prevEl: ".section_event .btn_prev",
-//   // },
-//   pagination: { el: ".pagination", type: "fraction" },
-//   slidesPerView: 4,
-//   spaceBetween: 12,
-// });
-
-const eventVisual = new Swiper(".section_event .swiper", {
-  a11y: {
-    enabled: true,
-    containerMessage: "프로모션 슬라이드 영역입니다.",
-    slideLabelMessage:
-      "총 {{slidesLength}}장의 슬라이드 중 {{index}}번 슬라이드 입니다.",
-    firstSlideMessage: "첫번째 슬라이드입니다.",
-    lastSlideMessage: "마지막 슬라이드입니다.",
-    paginationBulletMessage: "{{index}}번째 슬라이드로 이동합니다.",
-    containerRoleDescriptionMessage: "Carousel",
-    itemRoleDescriptionMessage: "Slide",
-    slideRole: "listitem",
+createSwiper(".section_visual .swiper", {
+  a11y: commonA11ySettings,
+  pagination: { el: ".visual_pagination", type: "fraction" },
+  navigation: {
+    nextEl: ".section_visual .btn_next",
+    prevEl: ".section_visual .btn_prev",
   },
+  slidesPerView: "auto",
+  centeredSlides: true,
+  autoplay: {
+    delay: 5000,
+  },
+  loop: true,
+  spaceBetween: 20,
+});
+
+createSwiper(".section_event .swiper", {
+  a11y: commonA11ySettings,
   slidesPerView: 3,
-  pagination: {
-    el: ".pagination",
-  },
+  pagination: { el: ".pagination" },
   navigation: {
     nextEl: ".section_event .btn_next",
     prevEl: ".section_event .btn_prev",
@@ -52,23 +41,10 @@ const eventVisual = new Swiper(".section_event .swiper", {
   spaceBetween: 12,
 });
 
-const bestArtistVisual = new Swiper(".section_best_artist .swiper", {
-  a11y: {
-    enabled: true,
-    containerMessage: "프로모션 슬라이드 영역입니다.",
-    slideLabelMessage:
-      "총 {{slidesLength}}장의 슬라이드 중 {{index}}번 슬라이드 입니다.",
-    firstSlideMessage: "첫번째 슬라이드입니다.",
-    lastSlideMessage: "마지막 슬라이드입니다.",
-    paginationBulletMessage: "{{index}}번째 슬라이드로 이동합니다.",
-    containerRoleDescriptionMessage: "Carousel",
-    itemRoleDescriptionMessage: "Slide",
-    slideRole: "listitem",
-  },
+createSwiper(".section_best_artist .swiper", {
+  a11y: commonA11ySettings,
   slidesPerView: 3,
-  pagination: {
-    el: ".pagination",
-  },
+  pagination: { el: ".pagination" },
   navigation: {
     nextEl: ".section_best_artist .btn_next",
     prevEl: ".section_best_artist .btn_prev",
@@ -76,29 +52,17 @@ const bestArtistVisual = new Swiper(".section_best_artist .swiper", {
   spaceBetween: 12,
 });
 
-const artistVisual = new Swiper(".section_artist .artist_area .swiper", {
-  a11y: {
-    enabled: true,
-    containerMessage: "프로모션 슬라이드 영역입니다.",
-    slideLabelMessage:
-      "총 {{slidesLength}}장의 슬라이드 중 {{index}}번 슬라이드 입니다.",
-    firstSlideMessage: "첫번째 슬라이드입니다.",
-    lastSlideMessage: "마지막 슬라이드입니다.",
-    paginationBulletMessage: "{{index}}번째 슬라이드로 이동합니다.",
-    containerRoleDescriptionMessage: "Carousel",
-    itemRoleDescriptionMessage: "Slide",
-    slideRole: "listitem",
-  },
+createSwiper(".section_artist .artist_area .swiper", {
+  a11y: commonA11ySettings,
   slidesPerView: 4,
-  pagination: {
-    el: ".pagination",
-  },
+  pagination: { el: ".pagination" },
   navigation: {
     nextEl: ".section_artist .btn_next",
     prevEl: ".section_artist .btn_prev",
   },
   spaceBetween: 12,
 });
+
 
 const pushButtons = document.querySelectorAll(".push_notification_btn");
 const selectBoxes = document.querySelectorAll(".select_box");
